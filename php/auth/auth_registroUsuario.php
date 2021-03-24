bc9edcb4090bbe8d5ae2f54cd28c1a05a2f599ae
HealingCoding <luismimimi326@gmail.com>
2021-03-23 21:10:29 -0600

    ...

--- php/auth/auth_registroUsuario.php
+++ php/auth/auth_registroUsuario.php
@@ -1,27 +1,27 @@
-<?php  include $_SERVER['DOCUMENT_ROOT']."\php\logic\verify.php";
+<?php  include $_SERVER['DOCUMENT_ROOT'].'\php\logic\verify.php';
 $verify = new verifier();
 if(isset($_POST['envioRegistro']))
 {
    if($_POST['nombreUsuario'] == '' || $_POST['pass'] == '' || $_POST['repass'] == '') {
      //Que no esten vacios.
-     header("Location:registrar_usuario.php?auth_registroUsuario=false")
+     header('Location:..\vistas\registrar_usuario.php?auth_registroUsuario=false');
 
-   }elseif(!($verify -> verify_pass($_POST['pass']))){
+   }elseif(!($verify -> verify_pass($_POST['pass']))) {
      //Que la contraseña cumpla los requisitos
-     header("Location:registrar_usuario.php?auth_registroUsuario=false")
+     header('Location:..\vistas\registrar_usuario.php?auth_registroUsuario=false');
 
-   }elseif($verify -> verify_repass($_POST['pass'],$_POST['repass'])){
+   }elseif($verify -> verify_repass($_POST['pass'],$_POST['repass'])) {
      //Que la contraseña sea confirmada
-     header("Location:registrar_usuario.php?auth_registroUsuario=false")
+     header('Location:..\vistas\registrar_usuario.php?auth_registroUsuario=false');
 
    }elseif(!($verify -> verify_user($_POST['nombreUsuario']))) {
      //Que el usuario cumpla los requisitors.
-     header("Location:registrar_usuario.php?auth_registroUsuario=false")
+     header('Location:..\vistas\registrar_usuario.php?auth_registroUsuario=false');
 
    }else{
      // El registro fue exitoso.
      // -> LLAMADA DE METODO SQL
-      header("Location:php\vistas\inicio.html?a=1")
+      header('Location:php\vistas\inicio.html?a=1');
    }
  }
 
