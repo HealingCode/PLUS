3dde1cfe1c13f0f61b1a42d0be575805cffc7413
HealingCoding <luismimimi326@gmail.com>
2021-06-30 03:06:03 -0500

    Archivo cascaron que llevara el código de consulta de cursos

---
+++ php/logic/busqueda.php
@@ -0,0 +1,31 @@
+<html>
+
+<link rel="stylesheet" type="text/css" href="../../vistas/CSS/style_general.css">
+<link rel="stylesheet" type="text/css" href="../../vistas/CSS/forms_style.css">
+<meta name = "viewport" content="user-scalable=0, width=device-width, initial-scale=1" >
+
+<body>
+
+<div class='contenedor'>
+
+
+    <HEADER>
+
+      <div id="inicioBoton">
+        <a href = "../../../index.php"><img src = '../../vistas/recursos/Logo.png'></a>
+      </div>
+
+    </HEADER>
+
+
+
+
+    <FOOTER>
+
+    </FOOTER>
+
+</div>
+
+</body>
+
+</html>
