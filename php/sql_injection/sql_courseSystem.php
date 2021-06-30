b3bfbff54b160f2f1c28108f1e29543ec0383040
HealingCoding <luismimimi326@gmail.com>
2021-06-30 03:08:10 -0500

    Métodos para llenar la tabla SQL Curso

---
+++ php/sql_injection/sql_courseSystem.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'\php\conecta.php';
+$conexion = conecta();
+
+class sqlCurso{
+
+function uploadZIP($path){ //almacena el path del archivo ZIP
+
+
+}
+
+}
+
+?>
