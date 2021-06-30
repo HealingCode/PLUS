c2ae7b0be06fe08538b5c98b80d0c34108181efe
HealingCoding <luismimimi326@gmail.com>
2021-06-30 03:05:41 -0500

    Formulario para subir cursos en ZIP

---
+++ php/forms/upload_curso.php
@@ -0,0 +1,41 @@
+<html>
+
+<link rel="stylesheet" type="text/css" href="../../vistas/CSS/style_general.css">
+<link rel="stylesheet" type="text/css" href="../../vistas/CSS/forms_style.css">
+<meta name = "viewport" content="user-scalable=0, width=device-width, initial-scale=1" >
+
+<body>
+
+<div class='contenedor'>
+
+
+<HEADER>
+
+      <div id="inicioBoton">
+        <a href = "../../../index.php"><img src = '../../vistas/recursos/Logo.png'></a>
+      </div>
+
+
+</HEADER>
+
+      <div class = 'registro'>
+        <div class ='form'>
+
+          <form method='POST' action="\php\auth\auth_uploadCurso.php" enctype="multipart/form-data">
+              <input type="file" name="cursoZip" id="zipToUpload">
+              <input type="submit" name="envioZip" value="subir">
+          </form>
+
+        </div>
+      </div>
+
+
+    <FOOTER>
+
+    </FOOTER>
+
+    </div>
+
+</body>
+
+</html>
