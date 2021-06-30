cacc3904adcc35aef43ac8fba9ba701c4aa1a8fb
HealingCoding <luismimimi326@gmail.com>
2021-06-30 03:07:24 -0500

    Métodos que manipularan el DATA.MODEL de SCORM 1.2

---
+++ php/sql_injection/sql_SCOSystem.php
@@ -0,0 +1,22 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'\php\conecta.php';
+$conexion = conecta();
+
+function selectCMIElement(){
+
+}
+
+function updateCMIElement(){
+
+}
+
+function initCMIElement(){
+  
+}
+
+function initSCO(){
+
+}
+
+
+ ?>
