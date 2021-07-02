2029401801c703b660f9eba23da1fa996eda8bda
HealingCoding <luismimimi326@gmail.com>
2021-07-01 22:22:39 -0500

    Borrado de directorio

---
+++ php/logic/fileManipulate.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class fileManipulate{
+/*
+ * php delete function that deals with directories recursively
+ */
+ function delete_directory($dirname) {
+          if (is_dir($dirname))
+            $dir_handle = opendir($dirname);
+      if (!$dir_handle)
+           return false;
+      while($file = readdir($dir_handle)) {
+            if ($file != "." && $file != "..") {
+                 if (!is_dir($dirname."/".$file))
+                      unlink($dirname."/".$file);
+                 else
+                      $this->delete_directory($dirname.'/'.$file);
+            }
+      }
+      closedir($dir_handle);
+      rmdir($dirname);
+      return true;
+ }
+}
+ ?>
