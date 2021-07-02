b597364a704c17ed14f9957d5e687622861e3cce
HealingCoding <luismimimi326@gmail.com>
2021-07-02 18:58:43 -0500

    creacion select y print cursos

--- php/sql_injection/sql_courseSystem.php
+++ php/sql_injection/sql_courseSystem.php
@@ -10,10 +10,44 @@ class sqlCurso{
 function insertCurso($name, $version, $launchDir, $zipDir) {
 $conexion = conecta();
     $id='';
+    $default = '../../vistas/default.jpg';
     mysqli_query($conexion,"INSERT INTO cursos VALUES (
-      '$id','$name','$version','$launchDir','$zipDir') ");
+      '$id','$name','$version','$launchDir','$zipDir','$default') ");
 
 }
+
+  function selectCursos(){
+    $conexion = conecta();
+    $cursos = mysqli_query($conexion, "SELECT * FROM cursos");
+
+      return $cursos;
+}
+
+  function printCursos(){
+/*
+  $cursos = $this -> selectCursos();
+  //print("<p>a</p>");
+
+    while($row = mysqli_fetch_array($cursos)){
+      echo '<a href = ../../../index.php> ';
+      echo '<div class = "curso">';
+      echo '<div class = "elemento" '.
+      'style = "background-image:url('.
+      $row["imgDir"].
+      ')">';
+       // lleva a la pagina que muestra cursos
+
+      echo '</div>';
+      echo '<div class = "pieTitular">';
+      echo $row["course_name"];
+      echo '</div>';
+      echo '</div></a>';
+
+    }
+*/
+
+
+  }
 
 }
 
