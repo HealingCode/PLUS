3242096474ec0dd812c7f144b41552035a918cd7
HealingCoding <luismimimi326@gmail.com>
2021-07-01 22:23:26 -0500

    Bug: mal funcionamiento de insertCurso()

--- php/sql_injection/sql_courseSystem.php
+++ php/sql_injection/sql_courseSystem.php
@@ -1,12 +1,17 @@
 <?php
 
 include_once $_SERVER['DOCUMENT_ROOT'].'\php\conecta.php';
-$conexion = conecta();
+
 
 class sqlCurso{
 
-function uploadZIP($path){ //almacena el path del archivo ZIP
 
+
+function insertCurso($name, $version, $launchDir, $zipDir) {
+$conexion = conecta();
+    $id='';
+    mysqli_query($conexion,"INSERT INTO cursos VALUES (
+      '$id','$name','$version','$launchDir','$zipDir') ");
 
 }
 
