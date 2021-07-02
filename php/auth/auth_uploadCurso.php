0555decbb59ad022cd0027286c596000ad2468c4
HealingCoding <luismimimi326@gmail.com>
2021-07-01 22:22:28 -0500

    Completado: Subir curso, descomprecion, subido a SQL y borrado de temp

--- php/auth/auth_uploadCurso.php
+++ php/auth/auth_uploadCurso.php
@@ -1,9 +1,12 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'\php\logic\verify.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'\php\sql_injection\sql_courseSystem.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/php/logic/lectorXML.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/php\logic\fileManipulate.php';
 $verify = new verifier();
-$sqlUser = new sqlCurso();
-
+$sqlCurso = new sqlCurso();
+$xmlLector = new lectorXML();
+$manipul = new fileManipulate();
 //--------------------
 // Variables de archivo ZIP
 //--------------------
@@ -50,9 +53,28 @@ if($banderaSubido){
 
 $zip = new ZipArchive;
 $zip -> open($dirToUpload);
-mkdir("../../cursos/SCORM/1.2/test");
+mkdir('../../cursos/SCORM/1.2/test');
 $zip -> extractTo('../../cursos/SCORM/1.2/test');
+
+
+//----------------------------------------
+//lEER EL MANIFESTO SCORM
+//----------------------------------------
+
+$pathManifest = '../../cursos/SCORM/1.2/test/imsmanifest.xml';
+$SCOdatos = $xmlLector -> getNameVerLauncher_XMLimsmanifest($pathManifest);
+$tituloCurso = $SCOdatos["nombre"];
+$version = $SCOdatos["version"];
+$launchDir = $SCOdatos["launcher"];
+
+mkdir("../../cursos/SCORM/". $version.'/' . $tituloCurso.'/');
+$zip -> extractTo("../../cursos/SCORM/". $version. '/'. $tituloCurso);
 $zip -> close();
+$sqlCurso -> insertCurso($tituloCurso,$version,$launchDir,$dirToUpload);
+
+//BORRAR LOS ARCHIVOS
+
+$manipul -> delete_directory('../../cursos/SCORM/1.2/test/');
 header("Location:../../../../index.php");
 }
 
