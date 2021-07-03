bafd4022b70f0585a742c7947f68e34bb5bb5afb
HealingCoding <luismimimi326@gmail.com>
2021-07-03 17:18:23 -0500

    Pagina de explorar curso

---
+++ vistas/paginaCurso.php
@@ -0,0 +1,32 @@
+<html>
+
+<link rel="stylesheet" type="text/css" href="../../vistas/CSS/style_general.css">
+<script src='vistas\js\registrarCurso.js' language='Javascript'></script>
+<meta name = "viewport" content="user-scalable=0, width=device-width, initial-scale=1" >
+
+<body>
+
+<div class='contenedor'>
+
+
+  <HEADER>
+    <div id="inicioBoton">
+      <a href = "../../../index.php"><img src = '../../vistas/recursos/Logo.png'></a>
+    </div>
+  </HEADER>
+
+  <div class = "cuerpo">
+
+    <b onclick="registrarCurso()"> Registrate al Curso </b>
+
+  </div>
+
+  <FOOTER>
+
+  </FOOTER>
+
+</div>
+
+</body>
+
+</html>
