5f4692e7979aed1f2f8964dd174f6741f05e9d45
HealingCoding <luismimimi326@gmail.com>
2021-07-08 19:02:28 -0500

    modificacion: Correcta llamada del directorio

--- vistas/js/registrarCurso.js
+++ vistas/js/registrarCurso.js
@@ -1,7 +1,7 @@
 function registrarCurso(idCurso,idStud) {
 
 const scohttp = new XMLHttpRequest();
-scohttp.open("POST", 'php\logic\registrarCurso.php',true);
+scohttp.open("POST", '../php/logic/registrarCurso.php',true);
 scohttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 scohttp.send("idCurso="+idCurso+"&nombre="+idStud);
 }
