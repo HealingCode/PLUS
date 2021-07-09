0e98da599685a36f7bb5b94fc3e71ea1953578b5
HealingCoding <luismimimi326@gmail.com>
2021-07-09 10:43:40 -0500

    Adición de refresh a la pagina

--- vistas/js/registrarCurso.js
+++ vistas/js/registrarCurso.js
@@ -4,4 +4,5 @@ const scohttp = new XMLHttpRequest();
 scohttp.open("POST", '../php/logic/registrarCurso.php',true);
 scohttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 scohttp.send("idCurso="+idCurso+"&nombre="+idStud);
+window.location.reload();
 }
