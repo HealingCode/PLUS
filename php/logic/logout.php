a044ce5f557e462a87cebdc384d5939c0c1118cc
HealingCoding <luismimimi326@gmail.com>
2021-11-15 16:18:28 -0600

    Nuevo logout script

--- php/logic/logout.php
+++ php/logic/logout.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-session_unset($_SESSION['login_user']);
-session_destroy();
+    session_unset();
+    session_destroy();
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
 header('Location:../../index.php');
  ?>
